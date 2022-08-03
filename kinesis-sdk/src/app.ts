589b4d8c46ef1013a23289e82233bc0c0ea64ce9
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-03 19:45:00 -0300

    ts

--- kinesis-sdk/src/app.ts
+++ kinesis-sdk/src/app.ts
@@ -1,3 +1,7 @@
 import { KinesisClient } from "@aws-sdk/client-kinesis";
+import 'dotenv/config';
+import 'os';
+
+process.env.S3_BUCKET
 
 const client = new KinesisClient({ region: "sa-east-1" });
