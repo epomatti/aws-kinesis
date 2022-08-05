d40f248b5846007b036ad076260a54f13f121b1d
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-04 22:26:15 -0300

    csv

--- kinesis-sdk/src/put.ts
+++ kinesis-sdk/src/put.ts
@@ -10,22 +10,27 @@ import {
   const DATA_STREAM = "device-stream";
   const client = new KinesisClient({ region: "sa-east-1" });
 
-  const text = "Hello!";
-  const bytes = new TextEncoder().encode(text);
 
-  const entry: PutRecordsRequestEntry = {
-    Data: bytes,
-    PartitionKey: "1"
+  const encoder = new TextEncoder()
+
+  // Uploads CSV values to be converted ot JSON
+  for (let i = 0; i < 1; i++) {
+    const csv = `${i};Thermometer;50.0;${new Date()}`;
+    const bytes = encoder.encode(csv);
+
+    const entry: PutRecordsRequestEntry = {
+      Data: bytes,
+      PartitionKey: "1"
+    }
+
+    const input: PutRecordsCommandInput = {
+      StreamName: DATA_STREAM,
+      Records: [entry]
+    }
+    const command = new PutRecordsCommand(input);
+    const response = await client.send(command);
+    console.log(response);
   }
-
-  const input: PutRecordsCommandInput = {
-    StreamName: DATA_STREAM,
-    Records: [entry]
-  }
-  const command = new PutRecordsCommand(input);
-  const response = await client.send(command);
-
-  console.log(response);
 
 
 })();
